9d8e4b471ed6e45b97f9bee398fad6c0ec289fd2
Lucas Culverhouse <lucasculverhouse@gmail.com>
2024-03-07 13:09:06 -0800

    clean-up whitespace and use vim as the manpager

--- config.fish
+++ config.fish
@@ -4,7 +4,7 @@ set -U EDITOR /usr/bin/nvim
 if status is-interactive
     # Commands to run in interactive sessions can go here
     alias fishcfg 'cd ~/.config/fish/'
-    
+
     # Common command aliases
     alias ls='ls -lGhvX --group-directories-first --color=auto'
     alias la='ls -lAGhvX --group-directories-first --color=auto'
@@ -12,12 +12,12 @@ if status is-interactive
     alias gs='git status'
     alias fst='head -n 1'
     alias lst='tail -n 1'
-    
+
     # Neovim Configurations
     alias vim="nvim"
     alias vi="nvim"
     alias vimrc="cd ~/.config/nvim"
-    
+
     # fzf Configurations
     #set -x FZF_DEFAULT_COMMAND "fd --type file --follow --hidden --exclude .git"
     if type -sq fzf
@@ -45,22 +45,22 @@ if status is-interactive
         end
         cd (fd --type d --exclude go . "$directory" -H | fzf || printf "$last")
     end
-    
+
     # note Configurations
     export NOTE_HOME="/home/lucas/Notes"
-    
+
     # Backups
     alias backup="rsync -a --info=progress2 --exclude=\".cache\" /home/lucas/ /run/media/lucas/Fedora\ Backup/"
-    
+
     # Reboot to Windows
-    function windows-reboot 
+    function windows-reboot
         set windows_entry $(sudo grep -i windows /etc/grub2.cfg | cut -d "'" -f 2)
         sudo grub2-reboot "$windows_entry" && sudo reboot
     end
-    
+
     # atuin Configurations
     atuin init fish --disable-up-arrow | source
-    
+
     # XDG environment
     set XDG_DATA_HOME $HOME/.local/share
     set XDG_CONFIG_HOME $HOME/.config
@@ -71,6 +71,16 @@ if status is-interactive
     function md
         pandoc $argv > /tmp/$argv.html
         xdg-open /tmp/$argv.html
+    end
+
+    # Use neovim as the manpage viewer
+    function man
+        if test -n "$argv[1]"
+            set manpage "$argv[1]"
+        else
+            echo -e 'What manual page do you want?\nFor example, try \'man man\'.' && exit 1
+        end
+        nvim -MR -c ":Man $manpage | only"
     end
 end
 
